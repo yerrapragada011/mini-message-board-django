f0582f142da318860b6ed30320f50f56f683ebec
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2025-04-18 15:30:52 -0400

    Fix db for deploy

--- mini_message_board/mini_message_board/settings.py
+++ mini_message_board/mini_message_board/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-import dj_database_url
 import os
 
 
@@ -79,7 +78,10 @@ WSGI_APPLICATION = 'mini_message_board.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
